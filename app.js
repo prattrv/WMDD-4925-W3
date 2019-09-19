bcc203898e012bacf7bac752da9010fb7268237e
EC2 Default User <ec2-user@ip-172-31-80-52.ec2.internal>
2019-09-19 20:38:58 +0000

    View engine

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const app = express()
 let staticMiddleware = express.static('public')
 app.use(staticMiddleware)
 
+app.set('view engine', 'ejs') // Embedded JS
+
 app.get('/file-test', (req, res) => {
     res.sendFile(__dirname + '/public/test.html')
+})
+
+app.get('/view-test', (req, res) => {
+    // We don't need the .ejs extention here.
+    // Always put templates in /views
+    res.render('view-test', {animal: "Whale"})
 })
 
 app.set('port', process.env.PORT || 8080)
